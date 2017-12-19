ad72d9899cc28f0e23e07efb12513082e9a3fb7a
Anton LARCHEY <totone56>
2017-12-19 15:11:11 +0100

    LER ACCENTS CER PA BIENS

--- app/src/main/java/lps2ima/kouize/QuestionActivity.java
+++ app/src/main/java/lps2ima/kouize/QuestionActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class QuestionActivity extends AppCompatActivity {
 
+    Question question;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/lps2ima/kouize/data/QuizzHelper.java
+++ app/src/main/java/lps2ima/kouize/data/QuizzHelper.java
@@ -1,8 +1,23 @@
 package lps2ima.kouize.data;
 
+import lps2ima.kouize.Quizz;
+
 /**
  * Created by toton on 13/12/2017.
  */
 
 public class QuizzHelper {
+
+    //Cette classe va servir à stocker le Quizz courant en dehors des vues, ça évite de faire passer des objets ou autres dans les Intents, c'est moins casse couille
+
+    static Quizz currentQuizz;
+
+    public static void setCurrentQuizz(Quizz quizz){
+        currentQuizz = quizz;
+    }
+
+    public static Quizz getCurrentQuizz(){
+        return currentQuizz;
+    }
+
 }
