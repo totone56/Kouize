adbd93313c24ef1ae53c183a07a49d3e8e701fed
Asouphie <anneso.krawczyk@gmail.com>
2017-12-19 15:11:35 +0100

    Modification des deux classes : Quizz et Question

--- app/src/main/java/lps2ima/kouize/Quizz.java
+++ app/src/main/java/lps2ima/kouize/Quizz.java
@@ -1,6 +1,10 @@
 package lps2ima.kouize;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by toton on 12/12/2017.
@@ -8,35 +12,52 @@ import java.util.ArrayList;
 
 public class Quizz {
 
-    private ArrayList<Question> questions;
-    int difficulté;
+    private String fournisseur;
+    @SerializedName(value = "redacteur", alternate = {"rédacteur"})
+    private String redacteur;
+    @SerializedName(value = "reponse", alternate = {"réponse"})
+    private String theme;
+    @SerializedName(value = "difficulte", alternate = {"difficulté"})
+    private int difficulte;
+    private Map<String, List<Question>> quizz;
 
-    public Quizz (int difficulté) {
-        this.difficulté = difficulté;
-        createQuestions(difficulté);
+    public String getFournisseur() {
+        return fournisseur;
     }
 
-    public Question getQuestion (int i){
-        return this.questions.get(i);
+    public void setFournisseur(String fournisseur) {
+        this.fournisseur = fournisseur;
     }
 
-    private void createQuestions(int difficulté) {
-        /* Todo Parser le fichier pour créer les questions*/
+    public String getRedacteur() {
+        return redacteur;
     }
 
-    public ArrayList<Question> getQuestions() {
-        return questions;
+    public void setRedacteur(String redacteur) {
+        this.redacteur = redacteur;
     }
 
-    public void setQuestions(ArrayList<Question> questions) {
-        this.questions = questions;
+    public String getTheme() {
+        return theme;
     }
 
-    public int getDifficulté() {
-        return difficulté;
+    public void setTheme(String theme) {
+        this.theme = theme;
     }
 
-    public void setDifficulté(int difficulté) {
-        this.difficulté = difficulté;
+    public int getDifficulte() {
+        return difficulte;
+    }
+
+    public void setDifficulte(int difficulte) {
+        this.difficulte = difficulte;
+    }
+
+    public Map<String, List<Question>> getQuizz() {
+        return quizz;
+    }
+
+    public void setQuizz(Map<String, List<Question>> quizz) {
+        this.quizz = quizz;
     }
 }
